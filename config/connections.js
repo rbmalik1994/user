579bd2f8f69a6df877c4e8b19c3bb078f864957c
Robin Malik <rbmalik1994@gmail.com>
2016-11-29 16:50:48 +0530

    add mysql connections

--- config/connections.js
+++ config/connections.js
@@ -40,12 +40,12 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
+   user_sql: {
+     adapter: 'sails-mysql',
+     host: 'localhost',
+     user: 'user', //optional
+     password: '1234', //optional
+     database: 'user' //optional
   // },
 
   /***************************************************************************
